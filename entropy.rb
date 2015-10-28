0a1f59b4f6fc210d91d55456e6818ad9f0e08ae7
Ryan Moore <moorer@udel.edu>
2015-10-28 18:36:39 -0400

    Add sliding window to graph

--- entropy.rb
+++ entropy.rb
@@ -17,6 +17,17 @@ def entropy str
   end
 end
 
+def mean arr
+  arr.reduce(:+) / arr.count.to_f
+end
+
+def windows arr, size, step
+  # I checked the arr.length - size for off by one errors
+  (0..arr.length-size).step(step).map do |start|
+    [(start*2 + size)/2.0, mean(arr[start, size])]
+  end
+end
+
 Signal.trap("PIPE", "EXIT")
 
 methods = File.join(File.expand_path("~"), "lib", "ruby", "ryan.rb")
@@ -37,20 +48,26 @@ opts = Trollop.options do
   opt(:database, "Input database (SILVA aligned)", type: :string,
       default: File.join("test_files", "database.fa"))
   opt(:outdir, "Output directory", type: :string,
-      default: "entropy")
+      default: File.join("output", "entropy"))
 end
 
 # database = Ryan.check_file(opts[:database], :database)
 Ryan.try_mkdir(opts[:outdir])
 
-SILVA_LEN = 50000
 mask_posns = [] # 0-based posns
 masked_sequences = []
 posn_entropy = []
+windowed_entropy = []
+
 entropy_f = File.join opts[:outdir], "entropy.txt"
 entropy_pdf = File.join opts[:outdir], "entropy.pdf"
+windowed_entropy_f = File.join opts[:outdir], "windowed_entropy.txt"
 rscript = ""
 rscript_f = File.join opts[:outdir], "entropy.r"
+
+WINDOW = 25
+STEP = 10
+SILVA_LEN = 50000
 
 Ryan.time_it("Create the masked sequences") do
   n = 0
@@ -82,38 +99,49 @@ end
 
 desc = "Calculate positional entropy of masked sequences"
 Ryan.time_it(desc) do
-  masked_sequences.map(&:chars).transpose.each_with_index do |chars, idx|
+  masked_sequences.map(&:chars).
+    transpose.
+    each_with_index do |chars, idx|
     pos = idx + 1
     posn_entropy << entropy(chars.join)
   end
 end
 
-Ryan.time_it("Plot it") do
+desc = "Get entropy windows"
+Ryan.time_it(desc) do
+  #posn_entropy.each_cons(20)
+
+  windowed_entropy = windows posn_entropy, WINDOW, STEP
+end
+
+Ryan.time_it("Plot entropy") do
   File.open(entropy_f, "w") do |f|
     posn_entropy.each_with_index do |ent, idx|
       f.puts [idx, ent].join "\t"
     end
   end
 
-  maxx = posn_entropy.count
-  minx = 0
-
-  maxy = posn_entropy.max
-  miny = 0
+  File.open(windowed_entropy_f, "w") do |f|
+    windowed_entropy.each do |x, y|
+      f.puts [x, y].join "\t"
+    end
+  end
 
   File.open(rscript_f, "w") do |f|
     rscript = %Q{
 pdf("#{entropy_pdf}", height = 8, width = 12)
 dat <- read.table("#{entropy_f}", header = F, sep = "\t")
-plot(dat$V1, dat$V2, col = "blue", pch = 20, xlab = "Position",
+plot(dat$V1, dat$V2, col = "gray48", pch = 20, xlab = "Position",
      ylab = "Entropy", type = "l")
+dat <- read.table("#{windowed_entropy_f}", header = F, sep = "\t")
+points(dat$V1, dat$V2, col = "red", type = "l", lwd = 3)
 invisible(dev.off())
 }
     f.puts rscript
   end
 
   Ryan.run_it "Rscript #{rscript_f}"
-  Ryan.run_it "open entropy.pdf"
+  Ryan.run_it "open #{entropy_pdf}"
 
   zero_posns = posn_entropy.select { |x| x.zero? }.count
   total_posns = posn_entropy.count.to_f
