9cb259ed2fd937d340cd4699d73fedac55300087
Ryan Moore <moorer@udel.edu>
2015-11-08 19:43:25 -0500

    Remove partial seqs

---
+++ remove_partial_seqs.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+
+Signal.trap("PIPE", "EXIT")
+
+methods = File.join(File.expand_path("~"), "lib", "ruby", "ryan.rb")
+require_relative methods
+require_relative "const"
+require_relative "pintail_methods"
+
+Ryan.req *%w[parse_fasta fail_fast parallel set]
+
+include FailFast::Assertions
+
+
+opts = Trollop.options do
+  banner <<-EOS
+
+  Prints out only full length sequences.
+
+  Options:
+  EOS
+
+  opt(:input, "input file", type: :string)
+  opt(:outdir, "Output directory", type: :string,
+      default: "output")
+end
+
+input = Ryan.check_file(opts[:input], :input)
+Ryan.try_mkdir(opts[:outdir])
+outf = File.join opts[:outdir], "#{input[:base]}.full_length.fa"
+
+num_full = 0
+num_partial = 0
+total = 0
+
+mask_posns = []
+
+FastaFile.open(Const::MASK).each_record do |head, seq|
+  seq.each_char.with_index do |char, idx|
+    if char == '*'
+      mask_posns << idx
+    end
+  end
+end
+
+first_five = mask_posns.take 5
+last_five = mask_posns.reverse.take(5).sort
+
+File.open(outf, "w") do |f|
+  FastaFile.open(opts[:input]).each_record do |head, seq|
+    missing_start = first_five.all? do |posn|
+      seq[posn] == "." || seq[posn] == "-"
+    end
+
+    missing_end = last_five.all? do |posn|
+      seq[posn] == "." || seq[posn] == "-"
+    end
+
+    if !missing_start && !missing_end
+      num_full += 1
+      f.printf ">%s\n%s\n", head, seq
+    else
+      num_partial += 1
+    end
+  end
+end
+
+total = num_full + num_partial.to_f
+s = "full: %.3f, partial: %.3f, total: %d\n" % [num_full / total,
+                                                num_partial / total,
+                                                total]
+Ryan.log s
